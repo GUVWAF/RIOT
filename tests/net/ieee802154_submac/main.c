47c1ad1f11dc1aa3bf9675e80cb802a8c078e03d
3JIouHoCoK <klimevdokimov@mail.ru>
2023-01-24 15:03:21 +0700

    ACK WORKED! Just try with gnrc_networking

--- tests/net/ieee802154_submac/main.c
+++ tests/net/ieee802154_submac/main.c
@@ -194,6 +194,8 @@ static ieee802154_dev_t *_reg_callback(ieee802154_dev_type_t type, void *opaque)
         break;
     case IEEE802154_DEV_TYPE_MRF24J40:
         printf("mrf24j40");
+    case IEEE802154_DEV_TYPE_SX126X:
+        printf("sx126x");
         break;
     }
 
